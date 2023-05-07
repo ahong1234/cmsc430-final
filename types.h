99fef26ee6de9f4a56d466759ee0165f3818e6f0
alex <alexhong240@gmail.com>
2023-05-07 19:33:40 -0400

    sd

--- types.h
+++ types.h
@@ -13,4 +13,4 @@ Types checkArithmetic(Types left, Types right);
 Types checkLogical(Types left, Types right);
 Types checkRelational(Types left, Types right);
 Types checkInt(Types left, Types right);
-Types checkIfElse(Types left, Types right);
+Types checkIf(Types express);
